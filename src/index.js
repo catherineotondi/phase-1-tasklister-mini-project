d2ab7ce8c4900a4701294c11ab8c32c9cacae796
catherineotondi <catherineotondi@gmail.com>
2022-08-27 17:56:34 +0300

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+  let formElement=document.querySelector("form")
+  formElement.addEventListener("submit", (e) => {
+
+    e.preventDefault()
+
+    listOfTask(e.target.new_task_description.value)
+    formElement.reset()
+  })
+
+  function listOfTask(tasks) {
+    let p = document.createElement("p")
+    p.textContent = `${tasks}  `
+    let btn = document.createElement("button")
+    btn.addEventListener("click", deleteList)
+    btn.textContent = " x "
+    p.appendChild(btn)
+  
+    document.getElementById("list").appendChild(p)
+    
+  }
+
+  function deleteList(e) {
+    e.target.parentNode.remove()
+  }
+
+
+
 });
